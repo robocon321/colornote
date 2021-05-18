c7efb5daa8f06de444de5f3b1bbd4300e5b2c38e
tuyen-cpu <tranquantuyen326326@gmail.com>
2021-05-18 14:57:18 +0700

    version1 update

--- app/src/main/java/com/example/colornote/activity/MainActivity.java
+++ app/src/main/java/com/example/colornote/activity/MainActivity.java
@@ -23,7 +23,7 @@ import com.example.colornote.fragment.DialogSortFragment;
 import com.example.colornote.util.Settings;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-// bbbbddddddv3
+// sưa version1
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     ViewPager viewPager;
