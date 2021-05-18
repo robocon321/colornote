746c6c3b5ebd35faac49d3f58021896e1eeb4039
robocon321 <robocon321n@gmail.com>
2021-05-18 13:45:17 +0700

    Test 1

--- app/src/main/java/com/example/colornote/activity/MainActivity.java
+++ app/src/main/java/com/example/colornote/activity/MainActivity.java
@@ -23,6 +23,7 @@ import com.example.colornote.fragment.DialogSortFragment;
 import com.example.colornote.util.Settings;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+// Haha
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     ViewPager viewPager;
