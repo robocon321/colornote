ae2c1a8543b1e86d69dd980769b7342ff550e173
robocon321 <robocon321n@gmail.com>
2021-05-21 00:00:39 +0700

    Add constant

--- app/src/main/java/com/example/colornote/util/Constant.java
+++ app/src/main/java/com/example/colornote/util/Constant.java
@@ -6,4 +6,14 @@ public class Constant {
     public static int RECYCLE_BIN = 1;
     public static int NON_COMPLETE = 2;
     public static int COMPLETE = 3;
+
+    // Settings VIEW
+    public static int LIST = 0;
+    public static int  DETAILS = 1;
+    public static int GRID = 2;
+    public static int LARGE_GRID = 3;
+
+    // Color default
+    public static String MAIN_COLOR = "#FFD54F";
+    public static String SUB_COLOR = "#FFC107";
 }
