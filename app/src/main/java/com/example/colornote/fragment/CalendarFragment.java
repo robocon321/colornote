1ceda915ffdc26aa1296aa7444a674c2db89e6df
NguyenTrungGit <18130258@st.hcmuaf.edu.vn>
2021-05-21 11:16:45 +0700

    Add file activity_search.xml, update activity_calendar.xml

--- app/src/main/java/com/example/colornote/fragment/CalendarFragment.java
+++ app/src/main/java/com/example/colornote/fragment/CalendarFragment.java
@@ -1,7 +1,10 @@
 package com.example.colornote.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
