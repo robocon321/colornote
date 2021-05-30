183dc075a6878b1d2b30453a973c07cdba4a3e37
robocon321 <robocon321n@gmail.com>
2021-05-30 23:14:14 +0700

    Refactor name layout fragment

--- app/src/main/java/com/example/colornote/fragment/ViewFragment.java
+++ app/src/main/java/com/example/colornote/fragment/ViewFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,7 +25,7 @@ public class ViewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.layout_view_option, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_option, container, false);
         view.findViewById(R.id.btnSortList).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
