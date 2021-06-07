b26836cf548e2ca473e743c81af9bd1c5defeff5
tuyen-cpu <tranquangtuyen326326@gmail.com>
2021-06-07 10:12:04 +0700

    .

--- app/src/main/java/com/example/colornote/fragment/MoreFragment.java
+++ app/src/main/java/com/example/colornote/fragment/MoreFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -16,12 +17,31 @@ import com.example.colornote.R;
 
 public class MoreFragment extends Fragment {
     @Nullable
-    @Override
+
+    Button btnMoreTrashCan,btnMoreArchive,btnMoreSettings,btnMoreTheme;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_more, container, false);
-
-
+        addControls(view);
+        addEvent();
         return view;
+
+    }
+
+    private void addControls(View view) {
+        btnMoreTrashCan = (Button) view.findViewById(R.id.btnMoreTrashCan);
+        btnMoreArchive = (Button) view.findViewById(R.id.btnMoreArchive);
+        btnMoreSettings = (Button) view.findViewById(R.id.btnMoreSettings);
+        btnMoreTheme = (Button) view.findViewById(R.id.btnMoreTheme);
+    }
+
+    private void addEvent() {
+        btnMoreTrashCan.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+    }
+});
     }
 
 }
